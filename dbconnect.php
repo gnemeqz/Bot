8c8079c0586924a0233bad615fc9158efe0be1a8
Detchanin Thongarsa <detchanin@hotmail.com>
2018-07-20 12:20:08 +0700

    20-04-61 (3)

--- dbconnect.php
+++ dbconnect.php
@@ -6,7 +6,14 @@ if($link){
 	print pg_last_error($link);
 }
 $sqlChk = pg_query($link, "SELECT id FROM php_car"); 
-$var =  $sqlChk;
-echo $var;
+  echo $sqlChk;
 
+
+$sql = pg_query($link,"SELECT id,brand FROM php_car WHERE id = $sqlChk");
+while($rs = pg_fetch_array($sql)) { 
+    if ($outp != "[") {$outp .= " ";} 
+        $outp .= 'ID  : ' . $rs["id"] . ' ';
+        $outp .= 'Brand  : ' . $rs["brand"] . ' ';} 
+
+    echo($outp); 
 ?>
